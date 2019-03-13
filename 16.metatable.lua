c1a62583b2d32066d32fe38adf205b8a3926fb0a
John_Q <qiucheng93@163.com>
2019-03-13 23:37:40 +0800

    update 03/13

--- 16.metatable.lua
+++ 16.metatable.lua
@@ -41,10 +41,92 @@ mytable = {"Lua","C#","Java","C++"}
 mymetatable = {
 __newindex = function (tab,key,value)
 	print("新增加了一个key值为: "..key.."， value为: "..value)
+	rawset(tab,key,value)  --把新增的键值对加入表中
 end
 }
+
+--[[
+newtable = {}
+mymetatable = {
+__newindex = newtable   --新的值会赋值给新的表
+}
+--]]
 
 setmetatable(mytable,mymetatable)
 
 mytable[1] = "xLua"
 mytable[5] = "TypeScript"
+
+print(mytable[5])
+
+--  4.__add对应+操作符，可以自定义表与表相加的行为，其他-*/%<>一样
+
+mytable = {"Lua","C#","Java","C++"}
+
+mymetatable = {
+__add = function (tab,newtab)
+	local mi = 0
+	for k,v in ipairs(tab) do
+		mi = mi+1
+	end
+
+	for k,v in ipairs(newtab) do
+		mi = mi+1
+		table.insert(tab,mi,v)
+	end
+	return tab
+end
+}
+
+setmetatable(mytable,mymetatable)
+
+secondtable = {"python","Javascript"}
+
+thirdtab = mytable + secondtable
+forthtab = secondtable + mytable
+
+for k,v in ipairs(forthtab) do
+	print(k,v)
+end
+
+--  5._call 当对一个表传参数的时候，可以把表当函数一样使用，则会调用call
+
+mytable = {"Lua","C#","Java","C++"}
+
+mymetatable = {
+__add = function (tab,newtab)
+	local mi = 0
+	for k,v in ipairs(tab) do
+		mi = mi+1
+	end
+
+	for k,v in ipairs(newtab) do
+		mi = mi+1
+		table.insert(tab,mi,v)
+	end
+	return tab
+end,
+__call = function (tab, arg1,arg2,arg3)
+	table.insert(tab, #tab+1,arg1)
+	print(arg1,arg2,arg3)
+end,
+__tostring = function (tab)
+	local str = ""
+	for k,v in ipairs(tab) do
+		str = str..v..","
+	end
+	return str
+end
+}
+
+setmetatable(mytable,mymetatable)
+
+mytable("Hello")
+
+for k,v in ipairs(mytable) do
+	print(k,v)
+end
+
+--  6.__tostring用于修改表的输出行为
+
+print(mytable)
