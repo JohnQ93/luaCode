3936d5f85909ac14b97210f6c3e290f84394e519
Qiu Cheng <qiucheng@sightp.com>
2019-03-20 09:41:13 +0800

    update oop

--- 20.oop.lua
+++ 20.oop.lua
@@ -7,12 +7,31 @@ end
 
 --Person:eat()
 
-function Person:new()
-	tab = {}
+function Person:new(o)
+	local tab = o or {}
 	setmetatable(tab,{__index = self})
+	--setmetatable(tab,self)
+	--self.__index = self
 	return tab
 end
 
 person1 = Person:new()
 
-print(person1:eat())
+print(person1:eat()) --冒号的作用，函数定义时，默认给函数添加隐藏的第一个参数self
+print(person1.name)
+
+person1.name="john"  --对属性进行修改不会对原型表做出修改，会赋值给tab;
+person1:eat()
+
+person2 = Person:new({weight=100})
+print(person2.weight)
+
+Student = Person:new()
+Student.grade = 1
+
+student1 = Student:new()
+
+student1:eat()
+print(student1.grade)
+
+-- student1继承于Student，Student继承于Person
